6786b2a552d5caa7a4b946fdc07a1e632b72c33f
a-asliCo <asli.ilhan@arts.ac.uk>
2025-02-12 00:48:18 +0000

    Fixed unescaped characters in About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,15 +2,18 @@ export default function About() {
     return (
       <div className="container text-center">
         <h2 className="text-3xl font-bold">About This App</h2>
-        <p className="text-lg text-gray-600 mt-4">Welcome ‼️ to the Glossary-Port, a dynamic tool designed to make learning, exploring, and managing words fun and interactive! Here's an overview of the exciting features you'll find:</p>
+      <p className="text-lg text-gray-600 mt-4">
+        This glossary helps students share important terms and their definitions.
+        It&apos;s designed to be simple and effective.
+      </p>
         <h3 className="text-3xl font-bold">Explore Words from the Letters Library 🌠:</h3>
         <p className="text-lg text-gray-600 mt-4">Watch words come alive as they move freely across the screen, symbolizing the fluidity of ideas. Hover over any word to pause its motion and learn more about it.</p>
         <h3 className="text-3xl font-bold">Dive Deeper 🤿:</h3>
-        <p className="text-lg text-gray-600 mt-4">Clicking on a word opens a Display Popup, where you can view its full details and meaning. You can also edit the word if there’s more to add or delete it if it’s no longer needed.</p>
+        <p className="text-lg text-gray-600 mt-4">Clicking on a word opens a Display Popup, where you can view its full details and meaning. You can also edit the word if there&apos;s more to add or delete it if it&apos;s no longer needed.</p>
         <h3 className="text-3xl font-bold">Edit and Personalize 🖊️:</h3>
         <p className="text-lg text-gray-600 mt-4">Customize any word using the Edit Popup feature. Update its details or redefine its concept to align with your understanding.</p>
         <h3 className="text-3xl font-bold">Add New Words ➕:</h3>
-        <p className="text-lg text-gray-600 mt-4">Have an idea or a new term to share? The "Add Word" button allows you to contribute to the collection. Enter the word, its concept, and an optional description, and watch it become part of the interactive experience.</p>
+        <p className="text-lg text-gray-600 mt-4">Have an idea or a new term to share? The &quot;Add Word&quot; button allows you to contribute to the collection. Enter the word, its concept, and an optional description, and watch it become part of the interactive experience.</p>
         <p className="text-lg text-gray-600 mt-4">This app is your playground for your research and concepts. Dive in, explore, and make the most of the interactive tools to enhance your learning journey! 🌟</p>
         <p className="text-lg text-gray-600 mt-4">If you have any questions or ideas for improvement, feel free to share them. Happy learning! 🚀</p>
       </div>
